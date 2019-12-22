9fda28a34659f5b4bdb6c1d610a6bcc00ca85803
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2019-12-22 10:49:47 +0100

    Update SingleProjectStage.vue

--- portfolio/.vuepress/components/SingleProjectStage.vue
+++ portfolio/.vuepress/components/SingleProjectStage.vue
@@ -9,7 +9,7 @@
     </div>  
   </div>
 
-<div class="content custom" :img src="{ `url(${post.frontmatter.thumbnail})` }">
+<div class="content custom" :img src="{{ `url(${post.frontmatter.thumbnail})` }}">
 
       <div class="info">
         <h2>{{ post.frontmatter.title }}</h2>
@@ -67,39 +67,6 @@
     .picture {
     width: 20px;
     height: auto;
-  }
-
-  .post {
-    position: relative;
-    width: 100%;
-    padding-top: 56.25%;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    margin-bottom: 5vw;
-    cursor: pointer;
-  }
-  .info {
-    position: absolute;
-    left: 0;
-    top: 2rem;
-    padding: 0.5rem 1rem;
-    background: rgba(255,255,255, 1);
-    max-width: 400px;
-  }
-  .info h2 {
-    display: inline-block;
-    width: auto;
-    font-size: 0.8rem;
-    font-weight: 700;
-    margin: 0;
-  }
-  .info span {
-    display: inline-block;
-    width: auto;
-    margin: 0;
-    margin-left: 0.5rem;
-    font-size: 0.8rem;
   }
 
 </style>
