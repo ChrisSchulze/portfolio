c027e234cf732925dddbc4aa27f88a2548ce8c80
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2019-12-24 17:33:10 +0100

    Update WorksList.vue

--- portfolio/.vuepress/components/WorksList.vue
+++ portfolio/.vuepress/components/WorksList.vue
@@ -1,6 +1,6 @@
 <template>
 
-      <svg viewBox="0 0 240 80" xmlns="http://www.w3.org/2000/svg">
+      <svg width="1000" height="500" xmlns="http://www.w3.org/2000/svg">
         <text>hallo</text>
       </svg>
 
