c78d57fe9ace67e760ab440b1e548483c0a76b59
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2019-12-26 18:39:53 +0100

    Update Layout.vue

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -163,6 +163,12 @@
   text-stroke: 0.5px rgba(210,210,210, 1);
   -webkit-text-stroke: 0.5px rgba(210,210,210, 1); 
   }
+  
+  .post .outline::after {
+  font-size: 1em;
+  content: "More +"; }
+  
+
              
   .outline:hover {
   color: #D1D1D2;
