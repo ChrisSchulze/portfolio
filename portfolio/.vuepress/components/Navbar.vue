7b2110fc477003614b89df88c37f2ff7f5227b8b
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2019-12-26 13:05:07 +0100

    Update Navbar.vue

--- portfolio/.vuepress/components/Navbar.vue
+++ portfolio/.vuepress/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="header" style="position: fixed; top: 0; left: 0; width: 100%;" >
+  <header class="header" style="position: fixed; top: 0; left: 0; width: 100%; height: 6rem" >
 
     <nav v-if="navLinks" class="navigation left desktop-nav">
       <ul>
