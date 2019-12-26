d3c245008ec0df30c998b8d251644b4a3d4dda58
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2019-12-26 09:11:27 +0100

    Update Layout.vue

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -160,8 +160,8 @@
   
   .outline {
   color: transparent !important;
-  text-stroke: 0.1rem rgba(210,210,210, 1);
-  -webkit-text-stroke: 0.1rem rgba(210,210,210, 1); 
+  text-stroke: 0.1rem rgba(210,210,210, 0.5);
+  -webkit-text-stroke: 0.1rem rgba(210,210,210, 0.5); 
   }
 
   h2 {
