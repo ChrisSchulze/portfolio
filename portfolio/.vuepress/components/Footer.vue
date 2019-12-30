eb1375fde535de8b6b5acfe08dbe87cbaf7dfd56
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2019-12-30 12:22:38 +0100

    Update Footer.vue

--- portfolio/.vuepress/components/Footer.vue
+++ portfolio/.vuepress/components/Footer.vue
@@ -1,25 +1,7 @@
 <template>
-  <span class="text">{{ $site.themeConfig.footer_copy }}</span>
+  <span class="text">{{ $site.themeConfig.footer_headline }}</span>
   
- <div>
-    <nav v-if="navLinks" class="navigation left desktop-nav">
-      <ul>
-        <router-link
-          v-for="nav in navLinks"
-          :key="nav.text"
-          v-if="nav.position === 'left' && !nav.external"
-          tag="li"
-          :to="nav.link"
-          active-class="active"
-          v-text="nav.text"
-          exact
-        />
-        <li v-for="nav in navLinks" v-if="nav.position === 'left' && nav.external">
-          <a :href="nav.link" target="_blank">{{ nav.text }}</a>
-        </li>
-      </ul>
-    </nav>
-</div>
+
 
  
 </template>
