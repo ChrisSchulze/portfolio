c6fc7c51c51b1eca482b3659cb83212e51159158
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2019-12-30 09:59:51 +0100

    Update WorksList.vue

--- portfolio/.vuepress/components/WorksList.vue
+++ portfolio/.vuepress/components/WorksList.vue
@@ -11,7 +11,7 @@
         <h1>{{ post.frontmatter.title }}<br>
               <span class="outline">{{ post.frontmatter.description }}</span>
         </h1>
-		<p><a class="animated-arrow" href="">
+		<a class="animated-arrow" href="">
                 <span class="the-arrow -left">
                   <span class="shaft"></span>
                 </span>
@@ -23,7 +23,7 @@
                     <span class="shaft"></span>
                   </span>
                 </span>
-              </a></p>
+              </a>
 
     </router-link>
   </div>
