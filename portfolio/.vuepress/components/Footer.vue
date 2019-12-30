604f9599745b4b0df11fc5889c6bd3fa1e6e4f03
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2019-12-30 12:02:45 +0100

    Update Footer.vue

--- portfolio/.vuepress/components/Footer.vue
+++ portfolio/.vuepress/components/Footer.vue
@@ -19,56 +19,18 @@
         </li>
       </ul>
     </nav>
+</div>
 
-
-    <nav v-if="navLinks" class="navigation right desktop-nav">
-      <ul>
-        <router-link
-          v-for="nav in navLinks"
-          :key="nav.text"
-          v-if="nav.position === 'right' && !nav.external"
-          tag="li"
-          :to="nav.link"
-          active-class="active"
-          v-text="nav.text"
-          exact
-        />
-        <li v-for="nav in navLinks" v-if="nav.position === 'right' && nav.external">
-          <a :href="nav.link" target="_blank">{{ nav.text }}</a>
-        </li>
-      </ul>
-    </nav>
- </div>
  
 </template>
 
 <script>
   export default {
-    props: {
-      logo: {
-        type: String,
-        required: false,
-      },
-      sticky: {
-        type: Boolean,
-        required: false,
-      }
-    },
-    data() {
-      return {
-        mobileNavActive: false
-      }
-    },
     computed: {
       navLinks() {
         return this.$site.themeConfig.nav
       },
     },
-    methods: {
-      toggleMobileNav() {
-        this.mobileNavActive = !this.mobileNavActive
-      }
-    }
   }
 </script>
 
