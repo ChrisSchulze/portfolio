4c2b7b97461f653f427cec5c6178dcdf07c565f4
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2020-01-01 10:32:15 +0100

    Create RelatedProjetcs.vue

---
+++ portfolio/.vuepress/components/RelatedProjetcs.vue
@@ -0,0 +1,53 @@
+<template>
+
+  <div class="header">
+
+        <h3>More Projects</h3>
+        <p><a href="{{ project_url }}">{{ project_name }}</a></p>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      project_url: {
+        type: String,
+        required: true
+      },
+      year: {
+        project_name: String,
+        required: true
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+  .header {
+    display: flex;
+    padding: 2rem 0;
+    width: 100%;
+    max-width: 600px;
+    margin: 0 auto 0 auto;
+  }
+
+  .label {
+    margin-bottom: 0.5rem;
+    font-weight: 700;
+  }
+
+  .value {
+    font-weight: 400;
+  }
+
+  .categories:after {
+    content: ', ';
+  }
+
+  .categories:last-of-type:after {
+    content: '';
+  }
+
+</style>
