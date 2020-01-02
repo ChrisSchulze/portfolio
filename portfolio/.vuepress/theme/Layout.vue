e17f118fdb4e6423f9fa0759a68253e72b076c58
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2020-01-02 12:45:44 +0100

    Update Layout.vue

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -154,7 +154,7 @@
         --padding-big: 80px;
         --padding-medium: 40px;
         --padding-small: 24px;
-        --padding-xsmall: 24px;
+        --padding-xsmall: 12px;
         }
   }
   
