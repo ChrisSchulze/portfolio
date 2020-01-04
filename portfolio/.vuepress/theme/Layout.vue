1406924eeeebf898d44d4e64b3930d4a173129c9
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2020-01-04 13:24:56 +0100

    Update Layout.vue

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -9,7 +9,7 @@
       <div
         v-if="$route.path === '/'"
         :style="{
-          marginTop: '10rem'
+          marginTop: '20rem'
         }"
       >
         <Content/>
@@ -18,7 +18,7 @@
           <!-- Single project view -->
           <div v-if="isSingleProject"
               :style="{
-              marginTop: '10rem'
+              marginTop: '20rem'
               }"
           >
       
