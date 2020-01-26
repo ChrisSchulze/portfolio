faed6b5cc9b0d797e3570b764e6a2bf64b0bfee9
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2020-01-26 10:48:17 +0100

    Update Layout.vue

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -292,7 +292,7 @@
 
   p {
     font-size: 2rem;
-    line-height: 2.8rem;
+    line-height: 2.875rem;
     letter-spacing: 1.2px;
     margin: auto auto auto auto;
     padding-bottom: var(--padding-big);
