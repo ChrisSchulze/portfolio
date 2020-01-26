981c53990add5729f5f9a193bc5c74561dba0b00
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2020-01-26 10:49:32 +0100

    Update Footer.vue

--- portfolio/.vuepress/components/Footer.vue
+++ portfolio/.vuepress/components/Footer.vue
@@ -9,7 +9,7 @@
         </div>
       </column>
       <column class="text">
-        <p class="hero">I'm always happy to meet new people. Give me a call <a class="contact" href="tel:+49-157-39240">+49 157 39 240 376</a> or send me an <a href="mailto:jan.christian.schulze@gmail.com" class="contact">email.</a></p>
+        <p class="hero">I'm always happy to meet new people. Give me a call <a class="bold" href="tel:+49-157-39240">+49 157 39 240 376</a> or send me an <a href="mailto:jan.christian.schulze@gmail.com" class="bold">email.</a></p>
       </column>
       <column class="logo">
         <img class="small" src="/upload/logo.png">
