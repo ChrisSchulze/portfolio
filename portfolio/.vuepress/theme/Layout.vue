a1eac830bfd489266e86725069d2ebf9d1e613b3
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2020-05-23 17:30:43 +0200

    Update Layout.vue

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -1,31 +1,30 @@
 <template>
 
   <div class="wrapper">
+  
       <Navbar :logo="$site.themeConfig.logo" />
+    
    <div class="container">
     
     <!-- Works list -->
-    
-    <transition name="fade" mode="out-in">
-        <div v-if="$route.path === '/'">
-        <Content/>
+      <transition name="moveInUp">
+      <div v-if="$route.path === '/'">
+       <Content/>
       </div>
 </transition>
-    
-    <!-- Single project view -->
-    
-        <transition name="fade" mode="out-in">
-              <div v-if="isSingleProject">
-                  <SingleProjectStage  
-                  :title="$page.frontmatter.title"
-                  :description="$page.frontmatter.description"
-                  />
-                <Content/>
-            <Arrow  
-            :project_name="$page.frontmatter.project_name"
-            :projecturl="$page.frontmatter.projecturl"
+          <!-- Single project view -->
+          
+<transition name="moveInUp">
+          <div v-if="isSingleProject">
+            <SingleProjectStage  
+            :title="$page.frontmatter.title"
+            :description="$page.frontmatter.description"
             />
-    </transition>
+              <div id="custom"><img :src="$page.frontmatter.thumbnail">
+              </div>
+                <Content/>
+           </div>   
+</transition>
          
         <Footer/>
 
@@ -72,53 +71,79 @@
 
 <style>
 
-  @font-face {
+@font-face {
   font-family: "PilatWideDemi";
   src: local("Pilat Wide Demi"), local("Pilat-Wide-Demi"),
     url("/upload/PilatWide-Demi.woff") format("woff");
   font-weight: normal;
   font-style: normal;
-  }
+}
 
-  @font-face {
+@font-face {
   font-family: "PilatBook";
   src: local("Pilat Book"), local("Pilat-Book"),
     url("/upload/Pilat-Book.woff") format("woff");
   font-weight: normal;
   font-style: normal;
+}
+
+  :root {
+    --color-black: #1c1c1c;
+    --color-highlight: rgba(249, 233, 172, 0.99);
+  }
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  *::-moz-selection {
+    background: var(--color-highlight);
+    color: var(--color-black);
+  }
+
+  *::-webkit-selection {
+    background: var(--color-highlight);
+    color: var(--color-black);
+  }
+
+  *::selection {
+    background: var(--color-highlight);
+    color: var(--color-black);
   }
 
   html {
   
-/* Font */
+      /* Font */
 
-  font-size: 16px;
-  line-height: 21.5px;
+      font-size: 16px;
+      line-height: 21.5px;
       
-/* Colors */
+      /* Colors */
       
-   background: #161618;
-   color: #818181;
+      background: #161618;
+      color: #595959;
       
-/* Spacing */
+      /* Spacing */
       
-   --spacing-xxl: 320px;
-   --spacing-xl: 240px;
-   --spacing-big: 160px;
+      --spacing-xxl: 320px;
+      --spacing-xl: 240px;
+      --spacing-big: 160px;
       --spacing-medium: 120px;
       --padding-small: 80px;
       --padding-xsmall: 40px;
       --padding-xxsmall: 16px;
   }
-      
+  
   @media (max-width: 900px) {
       html { 
         --spacing-xxl: 240px;
         --spacing-xl: 180px;
         --spacing-big: 120px;
         --spacing-medium: 80px;
-        --padding-small: 64px;
-        --padding-xsmall: 24px;
+        --padding-small: 40px;
+        --padding-xsmall: 40px;
         --padding-xxsmall: 16px;
         }
   }
@@ -127,34 +152,14 @@
       html {
         --spacing-xxl: 160px;
         --spacing-xl: 120px;
-        --spacing-big: 80px;
-        --spacing-medium: 64px;
+        --spacing-big: 64px;
+        --spacing-medium: 40px;
         --padding-small: 24px;
         --padding-xsmall: 24px;
-        --padding-xxsmall: 16px;
+        --padding-xxsmall: 8px;
         }
   }
   
-  :root {
-  
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  
-  --space-unit:  1em;
-  --space-xxxxs: calc(0.125 * var(--space-unit)); 
-  --space-xxxs:  calc(0.25 * var(--space-unit));
-  --space-xxs:   calc(0.375 * var(--space-unit));
-  --space-xs:    calc(0.5 * var(--space-unit));
-  --space-sm:    calc(0.75 * var(--space-unit));
-  --space-md:    calc(1.25 * var(--space-unit));
-  --space-lg:    calc(2 * var(--space-unit));
-  --space-xl:    calc(3.25 * var(--space-unit));
-  --space-xxl:   calc(5.25 * var(--space-unit));
-  --space-xxxl:  calc(8.5 * var(--space-unit));
-  --space-xxxxl: calc(10vw + var(--space-unit));
-}
-    
   body {
     font-family: PilatBook, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Noto Sans", "Ubuntu", "Droid Sans", "Helvetica Neue", sans-serif;  
     -webkit-font-smoothing: antialiased;
@@ -162,8 +167,7 @@
   }
   
   .container {
-    margin-top: var(--space-xxxxl);
-    padding: 0 5vw;
+    margin-top: var(--spacing-xl);
     }
 
   img {
@@ -171,12 +175,22 @@
     max-width: 100%;
     line-height: 0;
     margin: auto auto auto auto;
-    padding-bottom: var(--spacing-medium);
+    padding-bottom: var(--spacing-big);
   }
 
+  .container {
+    padding: 0 5vw;
+  }
+  
   .project-list {
     margin-top: var(--spacing-big) !important;
     }
+
+  .journal-list, .single-journal {
+    width: 800px;
+    max-width: 100%;
+    margin: 0 auto;
+  }
   
   a, a:link, a:visited {
     width: 100%;
@@ -185,35 +199,43 @@
     text-decoration: none;
   }
 
+  h3,h5,h6,p {
+    width: 100%;
+    max-width: 800px;
+  }
+
   h1 {
     font-family: "PilatWideDemi";
     width: 100%;
     max-width: 870px;
-    font-size: calc(5vw + 1rem);
-    line-height: calc(6vw + 1rem);
+    font-size: 6.25em;
+    line-height: 1em;
     font-weight: 700;
     color: #D1D1D2; 
     margin: 0 auto 0 auto;
-    padding-bottom: var(--spacing-medium);
+    padding-bottom: var(--spacing-big);
   }
   
-  .lead {     max-width: none !important;
-              }
+    @media (max-width: 900px) {
+      h1 {
+          font-size: 5em;
+          line-height: 1em;
+        }
+  }
+  
+    @media (max-width: 400px) {
+      h1 { 
+          font-size: 3em;
+          line-height: 1em;
+        }
+  }
   
   .outline {
-  letter-spacing: 0.03em;
-  color: #161618;
-
-    -webkit-text-stroke-width: 1px;
-    -webkit-text-stroke-color: #D1D1D2;
-}
-
-
-  h3,h5,h6,p {
-    width: 100%;
-    max-width: 870px;
+  color: transparent;
+  text-stroke: 0.5px rgba(210,210,210, 1);
+  -webkit-text-stroke: 0.5px rgba(210,210,210, 1); 
   }
-
+  
     .tight {
   padding-bottom: var(--padding-xsmall);
   }
@@ -236,8 +258,7 @@
     line-height: 0.825rem;
     letter-spacing: 2px;
     color: #D1D1D2;
-    margin: auto auto auto auto;
-    padding-bottom: var(--padding-xsmall);
+    margin: auto auto 0.25rem auto;
   }
   
   h4 {
@@ -252,10 +273,10 @@
 
   p {
     font-size: 2rem;
-    line-height: 2.6rem;
+    line-height: 2.875rem;
     letter-spacing: 1.2px;
     margin: auto auto auto auto;
-    padding-bottom: var(--spacing-medium);
+    padding-bottom: var(--spacing-big);
   }
   
   .bold {
@@ -265,20 +286,35 @@
   
   @media (max-width: 900px) {
       p {
-          font-size: 1.5em;
-          line-height: 1.2em;
+          font-size: 1.25em;
+          line-height: 1.5em;
         }
   }
   
   @media (max-width: 400px) {
       p { 
-          font-size: 1.25em;
-          line-height: 1.2em;
+          font-size: 1em;
+          line-height: 1.5em;
         }
   }
   
   .einleser {
   padding-bottom: var(--padding-small);
+  }
+  
+
+  pre {
+    background: var(--color-black);
+    padding: 1rem;
+    margin: 1rem 0;
+  }
+
+  code {
+    color: white;
+    background: var(--color-black);
+    font-size: 0.8rem;
+    padding: 0.05rem 0.25rem;
+    font-weight: 400;
   }
   
     .post {
@@ -288,22 +324,19 @@
     cursor: default;
   }
 
-/*** TRANSITIONS ***/
-.fade-enter {
-  transform: translateY(40px);
-  opacity: 0;
+.moveInUp-enter-active{
+  animation: fadeIn 0.6s ease-in-out;
+}
+@keyframes fadeIn{
+  0%{
+    opacity: 0;
+    transform: translateY(80px);
+  }
+  100%{
+    opacity: 1;
+    transform: translateY(0px);
+  }
 }
 
-.fade-enter-active {
-  transition: all 0.6s ease-out;
-}
 
-.fade-leave-active {
-  transition: all 0.1 ease-out;
-}
-
-.fade-leave-to {
-  transform: translateY(0px);
-  opacity: 0;
-}
 </style>
