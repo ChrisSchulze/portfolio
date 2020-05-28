7df57b0a5e854b3b1f5cbf7acdaa138be38266b9
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2020-05-28 14:27:34 +0200

    Update Layout.vue

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -194,12 +194,10 @@
   color: #161618;
   -webkit-text-stroke-width: 1px;
   -webkit-text-stroke-color: #D1D1D2;
-  }
-  
-  .tight {
   padding-bottom: var(--space-md);
   }
- 
+  
+
   h2 {
     font-family: "PilatWideDemi";
     width: 100%;
