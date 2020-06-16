26ada6d7327f50ad9bc2342d397f8886180ecdc0
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2020-06-16 22:05:43 +0200

    Update MoreProjects.vue

--- portfolio/.vuepress/components/MoreProjects.vue
+++ portfolio/.vuepress/components/MoreProjects.vue
@@ -38,22 +38,13 @@
 <style scoped>
 
 .all-projects {
-
+    width: 100%;
+    max-width: 870px;
 }
 
 .all-projects > .container {
-  /* We first create a flex layout context */
   display: flex;
-  
-  /* Then we define the flow direction 
-     and if we allow the items to wrap 
-   * Remember this is the same as:
-   * flex-direction: row;
-   * flex-wrap: wrap;
-   */
   flex-flow: row wrap;
-  
-  /* Then we define how is distributed the remaining space */
   justify-content: space-around;
   
   padding: 0;
