be26a9241baf4dff868d45416686304225d96c66
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2020-06-21 12:41:03 +0200

    Update Layout.vue

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -210,7 +210,7 @@
     font-family: "PilatWideDemi";
     width: 100%;
     max-width: 870px;
-    font-size: 0.875rem;
+    font-size: 2rem;
     line-height: 1rem;
     letter-spacing: 0.5px;
     color: #D1D1D2;
