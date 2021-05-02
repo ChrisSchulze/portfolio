1c823f8c53cb804a44622dd878753227056797b2
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2021-05-02 17:03:59 +0200

    Update Layout.vue

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -118,7 +118,7 @@
   --space-xs:    calc(0.5 * var(--space-unit));
   --space-sm:    calc(0.75 * var(--space-unit));
   --space-md:    calc(1.25 * var(--space-unit));
-  --space-lg:    calc(2 * var(--space-unit));
+  --space-lg:    calc(2.25 * var(--space-unit));
   --space-l:     calc(2.75 * var(--space-unit));
   --space-xl:    calc(3.75 * var(--space-unit));
   --space-xxl:   calc(5 * var(--space-unit));
