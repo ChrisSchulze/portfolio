7b8bf7bcc64aadffbfed243d13636245ecabb508
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2021-05-02 10:09:33 +0200

    Update Layout.vue

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -287,7 +287,7 @@
     margin-bottom: var(--space-xxl);
     height: 70vh;
     border-radius: 24px;
-    box-shadow: inset 0 0 0 1000px rgba(0,0,0,0.9);
+    box-shadow: inset 0 0 0 1000px rgba(0,0,0,0.5);
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
