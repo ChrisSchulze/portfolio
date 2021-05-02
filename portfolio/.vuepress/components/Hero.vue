3b97e8a26fdff2f60b91cc1bfc0ca619098405ac
ChrisSchulze <57041536+ChrisSchulze@users.noreply.github.com>
2021-05-02 17:52:26 +0200

    Update Hero.vue

--- portfolio/.vuepress/components/Hero.vue
+++ portfolio/.vuepress/components/Hero.vue
@@ -17,7 +17,7 @@ export default {
   .title {
     font-family: "PilatWideDemi";
     text-align: center;
-    width: 800px;
+    width: 640px;
     max-width: 100%;
     font-size: var(--space-lg);
     line-height: var(--space-lg);
